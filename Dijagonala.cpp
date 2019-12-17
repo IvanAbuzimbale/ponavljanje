b4686dfc369799984c954829ad68a82a44f196d5
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2019-12-17 14:02:58 +0100

    Add files via upload

---
+++ Dijagonala.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int n, brojac;
+	cout<<"Unesi broj: ";
+	cin>>n;
+	int a[n][n];
+	for (int i=0;i<n;i++){
+		for (int j=0;j<n;j++){
+	a[i][j]=brojac;	
+	cout<<a[i][j]<<" ";
+	brojac++;
+	}	
+	cout<<endl;
+	}
+	
+
+	system("PAUSE");
+	
+}
